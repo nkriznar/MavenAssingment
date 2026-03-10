4f55b1a37c8a4785e65568f278bc6d84cd73fac3
nkriznar <nkriznar301@gmail.com>
2026-03-10 19:48:19 -0300

    Add version banner to Main.java - testing auto-build

--- src/main/java/com/smu/mscda/Main.java
+++ src/main/java/com/smu/mscda/Main.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("=== MavenAssignment v3.11.8 (Jenkins CI/CD Demo) ===");
         System.out.println("This program will capitalize the input string and generate MD5 hex! \n");
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a string: ");
@@ -18,7 +19,7 @@ public class Main {
             String capitalizedString = WordUtils.capitalize(inputString);
             System.out.println("\n Capitalized string is: " + capitalizedString);
 
-            //Generate MD5 hex of the input string
+            // Generate MD5 hex of the input string
             String md5Hex = generateMD5Hex(capitalizedString);
             System.out.println("\n MD5 Hex is: " + md5Hex);
 
